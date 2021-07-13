c814d1f83372d72e2ebd702c2eaaedea0fad37ea
islomaliy <kislombek71@gmail.com>
2021-07-13 19:40:11 +0500

    Hero finished

--- src/components/Hero/HeroElements.js
+++ src/components/Hero/HeroElements.js
@@ -11,11 +11,11 @@ export const HeroContainer = styled.div`
 export const HeroContent = styled.div`
     height: calc(100vh, -80px);
     max-height: 100%;
-    width: 100vw;
+    /* width: 100vw; */
     padding: 0rem calc((100vw - 1300px) / 2);
 `;
 
-const HeroItems = styled.div `
+export const HeroItems = styled.div `
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -25,4 +25,40 @@ const HeroItems = styled.div `
     padding: 0 2rem;
     width: 650px;
     color: #fff;
+    text-transform: uppercase;
+    line-height: 1;
+    font-weight: bold;
+
+    @media screen and (max-width: 650px){
+        width: 100%;
+    }
 `;
+
+export const HeroH1 = styled.h1`
+    font-size: clamp(2.5rem, 10vw, 5rem);
+    margin-bottom: 1rem;
+    box-shadow: 3px 5px #e9ba23;
+    letter-spacing: 3px;
+`;
+
+export const HeroP = styled.p`
+    font-size: clamp(2rem, 2.5vw, 3rem);
+    margin-bottom: 2rem;
+`;
+
+export const HeroBtn = styled.button`
+    font-size: 1.4rem;
+    padding: 1rem 4rem;
+    border: none;
+    background: #e31837;
+    color: #fff;
+    transition: 0.2s ease-out;
+
+    &:hover {
+        background: #ffc500;
+        transition: 0.2s ease-out;
+        cursor: pointer;
+        color: #000;
+    }
+`;
+
